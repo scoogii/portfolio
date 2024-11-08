437f29ca076a0231bd01455d7ba5bd929507451b
scoogii <christian.nguyen6@outlook.com>
2024-11-08 17:17:42 +1100

    resume button in name of home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
-import { Box } from "@mui/joy";
+import { Box, Button, Tooltip } from "@mui/joy";
 import styles from "./page.module.css";
 import SocialButtons from "./_components/SocialButtons";
 import Nav from "./_components/Nav";
+import Link from "next/link";
 
 export default function Home() {
   const introCardStyle = {
@@ -11,6 +12,18 @@ export default function Home() {
     alignItems: "center",
     textAlign: "center",
     gap: { xs: "25px", sm: "20px", md: "20px", lg: "30px" },
+  };
+
+  const nameButtonStyle = {
+    background: "transparent",
+    padding: "5px",
+    color: "inherit",
+    font: "inherit",
+    ":hover": {
+      background: "transparent",
+      transition: "all 200ms ease-in",
+      transform: "scale(1.1)",
+    },
   };
 
   return (
@@ -24,7 +37,14 @@ export default function Home() {
             gap: "15px",
           }}
         >
-          <h1 style={{ float: "left" }}>Hi! I&apos;m Christian</h1>
+          <h1 style={{ float: "left" }}>
+            Hi! I&apos;m{" "}
+            <Tooltip title="Download my resume!" arrow sx={{ font: "inherit" }}>
+              <Button sx={nameButtonStyle} href="/" component={Link}>
+                Christian
+              </Button>
+            </Tooltip>
+          </h1>
           <h1 className={styles.rotate} style={{ float: "right" }}>
             👋
           </h1>
