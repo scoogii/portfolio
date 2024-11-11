7e21ae4ff889922c2e633da0b6cdcf0fa0308e16
scoogii <christian.nguyen6@outlook.com>
2024-11-11 20:48:11 +1100

    wip for miscellaneous page

--- app/misc/page.jsx
+++ app/misc/page.jsx
@@ -1,10 +1,25 @@
+import { Box } from "@mui/joy";
+import styles from "../page.module.css";
 import Nav from "../_components/Nav";
 
 const Misc = () => {
+  const tempStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    fontSize: "70px",
+  };
+
   return (
     <>
-      <Nav />
-      <div>Misc</div>
+      <main className={styles.main}>
+        <Nav />
+        <Box className={styles.loading} sx={tempStyle}>
+          WIP
+        </Box>
+      </main>
     </>
   );
 };
