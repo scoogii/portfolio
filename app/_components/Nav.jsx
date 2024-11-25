d23498dade82f68287205ca1f719638048405fa8
scoogii <christian.nguyen6@outlook.com>
2024-11-25 12:28:36 +1100

    add burger icon to small screne size in nav

--- app/_components/Nav.jsx
+++ app/_components/Nav.jsx
@@ -1,6 +1,7 @@
-import { Box, Button } from "@mui/joy";
+import { Box, Button, Dropdown, Menu, MenuButton, MenuItem } from "@mui/joy";
 import Link from "next/link";
 import HomeIcon from "@mui/icons-material/Home";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Nav = () => {
   const navStyle = {
@@ -26,6 +27,23 @@ const Nav = () => {
       transform: "scale(1.1)",
     },
     font: "inherit",
+    display: { xs: "none", sm: "auto" },
+  };
+
+  const menuButtonStyle = {
+    marginLeft: "-15px",
+    ":hover": {
+      background: "#354a75",
+    },
+  };
+
+  const menuStyle = {
+    background: "#2b3c5e",
+    border: "1px solid #1a2438",
+  };
+
+  const menuItemStyle = {
+    color: "white",
   };
 
   return (
@@ -39,6 +57,24 @@ const Nav = () => {
       <Button sx={navButtonStyle} href="/misc" component={Link}>
         Misc
       </Button>
+
+      <Dropdown>
+        <MenuButton variant="plain" size="lg" sx={menuButtonStyle}>
+          <MenuIcon sx={{ color: "white" }} />
+        </MenuButton>
+
+        <Menu variant="plain" sx={menuStyle}>
+          <MenuItem sx={menuItemStyle} href="/">
+            Home
+          </MenuItem>
+          <MenuItem sx={menuItemStyle} href="/projects">
+            Projects
+          </MenuItem>
+          <MenuItem sx={menuItemStyle} href="/misc">
+            Misc
+          </MenuItem>
+        </Menu>
+      </Dropdown>
     </Box>
   );
 };
