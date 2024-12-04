522882005410b7eefce11383cb9f0eb89c558495
scoogii <christian.nguyen6@outlook.com>
2024-12-04 19:25:33 +1100

    fix wave spacing in landing page text

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 export default function Home() {
   const introCardStyle = {
     display: "flex",
+    width: "100%",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
@@ -29,15 +30,19 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Nav />
+
       <Box sx={introCardStyle}>
         <div
           style={{
-            clear: "both",
             display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
             gap: "15px",
+            width: "80%",
           }}
         >
-          <h1 style={{ float: "left" }}>
+          <h1>
             Hi! I&apos;m{" "}
             <Tooltip title="Download my resume!" arrow sx={{ font: "inherit" }}>
               <Button
@@ -51,15 +56,17 @@ export default function Home() {
               </Button>
             </Tooltip>
           </h1>
-          <h1 className={styles.rotate} style={{ float: "right" }}>
-            👋
-          </h1>
+
+          <h1 className={styles.rotate}>👋</h1>
         </div>
+
         <h3>Hobby mestake maker. Black belt in Google.</h3>
+
         <h4 style={{ fontStyle: "italic" }}>
           &quot;If you don&apos;t have any problems, make your own.&quot;
         </h4>
       </Box>
+
       <SocialButtons />
     </main>
   );
